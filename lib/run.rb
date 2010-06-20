99869cff2e53e4e7db03bd732a075767185d6846
Jarmo Pertman <jarmo.p@gmail.com>
2010-06-20 18:11:55 +0300

    initial commit

---
+++ lib/run.rb
@@ -0,0 +1,21 @@
+require "sinatra"
+require "haml"
+raise "wrong ruby version" unless RUBY_VERSION == "1.9.1"
+
+class MyClass
+end
+
+configure do
+  def b
+    @@b ||= MyClass.new
+  end
+end
+
+get "/" do
+  haml :index
+end
+
+get '/scripts/pmdb.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :pmdb, :syntax => :scss
+end
