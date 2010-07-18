d7c33899c2594575469624b33b7ca3812e1bd789
Jarmo Pertman <jarmo.p@gmail.com>
2010-07-18 19:07:52 +0300

    added details panels

--- lib/run.rb
+++ lib/run.rb
@@ -1,6 +1,7 @@
+gem "eventmachine", "=0.12.8"
 require "sinatra"
 require "haml"
-raise "wrong ruby version" unless RUBY_VERSION == "1.9.1"
+#raise "wrong ruby version" unless RUBY_VERSION == "1.9.1"
 
 class MyClass
 end
@@ -15,7 +16,7 @@ get "/" do
   haml :index
 end
 
-get '/scripts/pmdb.css' do
+get '/css/pmdb.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :pmdb, :syntax => :scss
 end
