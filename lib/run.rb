1de8dbeb2fec1b1a82000358e882a7c68a686e59
Jarmo Pertman <jarmo.p@gmail.com>
2011-11-19 15:49:00 +0200

    use modular sinatra app

--- lib/run.rb
+++ lib/run.rb
@@ -1,24 +1,29 @@
 require "bundler"
 Bundler.setup
-require "sinatra"
+require "sinatra/base"
 require "haml"
 require 'compass'
 
-configure do
-  Compass.configuration do |config|
-    config.project_path = File.dirname(__FILE__)
-    config.sass_dir = 'views'
+class PMDb < Sinatra::Base
+  configure do
+    Compass.configuration do |config|
+      config.project_path = File.dirname(__FILE__)
+      config.sass_dir = 'views'
+    end
+
+    set :haml, {:format => :html5}
+    set :scss, Compass.sass_engine_options
+    enable :logging
   end
 
-  set :haml, {:format => :html5}
-  set :scss, Compass.sass_engine_options
-end
+  get "/" do
+    haml :index
+  end
 
-get "/" do
-  haml :index
-end
+  get '/css/pmdb.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    scss :pmdb
+  end
 
-get '/css/pmdb.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  scss :pmdb
+  run! if app_file == $0
 end
