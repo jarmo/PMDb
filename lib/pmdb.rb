225a227ef87687369941b3fca1d00543827810fd
Jarmo Pertman <jarmo.p@gmail.com>
2011-11-20 14:52:27 +0200

    add initial MovieFinder and IMDb

--- lib/pmdb.rb
+++ lib/pmdb.rb
@@ -7,15 +7,81 @@ require 'compass'
 require 'yaml'
 require 'pathname'
 
+class IMDb
+  def initialize(file)
+    @file = file
+  end
+
+  def parse
+    @url = parse_url_from_file
+    name = ""
+    unless @url
+      name = parse_name_and_year_from_dir
+      @url = url_from name
+    end
+    parse_name_and_url_from_dir
+    @name, @score, @votes, @genres = parse_data_from_imdb
+  end
+
+  private
+
+  def parse_url
+    return unless file.extname =~ /\.nfo$/i
+
+    urls = URI.extract(@file.read, "http")
+    urls.detect {|url| url =~ /imdb/i}
+  end
+
+  def parse_name_from_dir
+    dir = file.dirname.basename.to_s.gsub(/['"]/, "")
+    year = dir.scan(/\s?((?:19|20)\d{2})/).flatten.last
+
+    name = ""
+    if year
+      name_without_year = dir.gsub(/\s?#{year}.*/, "")
+      unless name_without_year.empty?
+        name = "#{name_without_year} (#{year})"
+      end
+    end
+    @url = URI.escape("http://akas.imdb.com/find?s=all&x=0&y=0&q=#{@name}")
+  end
+end
+
 class MovieFinder
   def initialize(options)
     @options = options
   end
 
-  def find
-    dirs = @options["dirs"].reduce([]) do |memo, dir|
+  def movies
+    parse_imdb_info(objectify(files(directories)))
+  end
+
+  private
+  
+  def directories
+    @options["dirs"].reduce([]) do |memo, dir|
       memo << Pathname.new(dir).children.select(&:directory?)
     end.flatten
+  end
+
+  def files(dirs)
+    dirs.reduce([]) do |memo, dir|
+      file = dir.children.detect do |f|
+        next unless f.file?
+        f.extname =~ /\.(nfo|mkv|avi|mp4)$/i
+      end
+      memo << file
+    end
+  end
+
+  def objectify(files)
+    files.map do |f|
+      {imdb: IMDb.new(f), path: f.to_s, mtime: f.mtime}
+    end
+  end
+
+  def parse_imdb_info(objs)
+
   end
 end
 
@@ -47,7 +113,7 @@ class PMDb < Sinatra::Base
 
   get "/" do
     p settings.pmdb
-    puts MovieFinder.new(settings.pmdb).find
+    puts MovieFinder.new(settings.pmdb).movies
     haml :index
   end
 
